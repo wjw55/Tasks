ea6011bb5a5f82084b225fb3803dda9883282788
wjw55 <wjw50100@gmail.com>
2026-02-20 17:08:34 +0800

    Extract welcome message

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Task manager (using streams)");
+        printWelcomeMessage();
         Datamanager dataManager = new Datamanager("./data/data.txt");
         ArrayList<Task> tasksData = dataManager.loadData();
 
